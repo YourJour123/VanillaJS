9d883277c4a90f7ddfd52973aad5404f52a54066
YourJour123 <bonjour010728@gmail.com>
2023-10-22 23:56:11 +0900

    2023-10-22

--- JS/background.js
+++ JS/background.js
@@ -1,9 +1,16 @@
-const images = ["IMG1.jpg", "IMG2.jpg", "IMG3.jpg"];
+const images = [
+  "background1.gif",
+  "background2.gif",
+  "background3.gif",
+  "background4.gif",
+  "background5.gif",
+];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
 bgImage.src = `IMG/${chosenImage}`;
+bgImage.id = "background-image";
 
 document.body.appendChild(bgImage);
