756d547b9fbe7d4026945f4e8a3d4be052ab3427
YourJour123 <bonjour010728@gmail.com>
2023-11-12 23:43:59 +0900

    2023-11-12

--- JS/weather.js
+++ JS/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "309df4c7b7c9ef43d25ecb8d9da562dd";
 function onGeo(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(`현재 위치: 위도 ${lat}, 경도 ${lon}`);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
